a2335293f8cf197fed826e7cff36bade7c5e55d3
Unkovic Nemanja <unkovic.nemanja@yahoo.com>
2015-11-13 23:09:45 +0100

    Refactor middleware

--- server.js
+++ server.js
@@ -2,16 +2,7 @@ var express = require("express");
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-    requireAuthentication: function(res, req, next) {
-        console.log("private route hit!");
-        next();
-    },
-    logger: function(req, res, next) {
-        console.log("Request: " + new Date().toString()+ " " + req.method + " " + req.originalUrl);
-        next();
-    }
-};
+var middleware = require("./middleware.js");
 
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
